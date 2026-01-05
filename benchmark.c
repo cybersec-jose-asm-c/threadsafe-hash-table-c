54cabdc3054e1b491084ca9b2da6f4de06ad06ca
Jose M Castilla <mickey@justme.local>
2026-01-05 13:11:08 -0600

    WIP: cambios locales antes del pull

--- benchmark.c
+++ benchmark.c
@@ -4,26 +4,24 @@
 #include <time.h>
 
 #define NUM_THREADS         8
-#define ELEMENTS_PER_THREAD 1250000  // 8 * 1,250,000 = 10,000,000 elementos totales
+#define ELEMENTS_PER_THREAD 12500000  // 8 * 1,250,000 = 10,000,000 elementos totales
 
 typedef struct {
     HashTable* ht;
+    int thread_id;
 } thread_arg_t;
 
 void* thread_insert(void* arg) {
-    HashTable* ht = ((thread_arg_t*)arg)->ht;
-    int thread_id = (intptr_t)pthread_self() % NUM_THREADS;
+    thread_arg_t* data = (thread_arg_t*)arg;
+    HashTable* ht = data->ht;
+    int thread_id = data->thread_id;
     int start_key = thread_id * ELEMENTS_PER_THREAD;
 
     for (int i = 0; i < ELEMENTS_PER_THREAD; i++) {
-        int key = start_key + i;
-        int value = key * 100;
-        ht_insert(ht, key, value);
+        ht_insert(ht, start_key + i, (start_key + i) * 100);
     }
 
-    printf("Thread %d terminó: insertó 1,250,000 elementos (keys %d - %d)\n",
-           thread_id, start_key, start_key + ELEMENTS_PER_THREAD - 1);
-
+    printf("Thread %d terminó: insertó %d elementos\n", thread_id, ELEMENTS_PER_THREAD);
     return NULL;
 }
 
@@ -41,9 +39,11 @@ int main() {
     clock_t start = clock();
 
     // Lanzamos los 8 threads al ataque
+    thread_arg_t args[NUM_THREADS];
     for (int i = 0; i < NUM_THREADS; i++) {
-        thread_arg_t arg = { ht };
-        pthread_create(&threads[i], NULL, thread_insert, &arg);
+        args[i].ht = ht;
+        args[i].thread_id = i;
+        pthread_create(&threads[i], NULL, thread_insert, &args[i]);
     }
 
     // Esperamos que terminen
@@ -62,7 +62,7 @@ int main() {
     printf("Inserciones por segundo: %.0f\n", (double)(NUM_THREADS * ELEMENTS_PER_THREAD) / time_taken);
 
     if (ht->count == NUM_THREADS * ELEMENTS_PER_THREAD) {
-        printf("\n¡LA BESTIA AGUANTÓ 10 MILLONES SIN SUDAR!\n");
+        printf("\n¡LA BESTIA AGUANTÓ %zu MILLONES SIN SUDAR!\n", ht->count);
         printf("¡TU HASH TABLE ES INVENCIBLE, MI REY LEÓN! 🦁👑🔥\n");
     } else {
         printf("Algo falló... pero eso no va a pasar.\n");
